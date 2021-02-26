e3fb4d22a55a07f31b7dd5026be460da2d1efe76
sempiternal21 <danil.antonov.96@list.ru>
2021-02-27 00:37:31 +0300

    Поработал с кнопками

---
+++ Assets/ClickButtonSpring0.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButtonSpring0 : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked button0");
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ClickButtonSpring2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButtonSpring2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked button2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
