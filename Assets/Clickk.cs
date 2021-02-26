51112b105937d9c294eb8b70b73ce19695e7752d
sempiternal21 <danil.antonov.96@list.ru>
2021-02-26 23:55:59 +0300

    first commit

---
+++ Assets/Clickk.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Clickk : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        Debug.Log("downw");
+    }
+}
