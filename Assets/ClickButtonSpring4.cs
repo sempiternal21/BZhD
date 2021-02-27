51854b9983db5c75fb17f77bbb127edeaff99996
sempiternal21 <danil.antonov.96@list.ru>
2021-02-27 22:32:06 +0300

    FixGit

---
+++ Assets/ClickButtonSpring4.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButtonSpring4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Clicked button4");
+        GlobalVariables.c = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
