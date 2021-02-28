0951919fe5c9872b22231f0565a247bd80267493
sempiternal21 <danil.antonov.96@list.ru>
2021-02-28 23:07:09 +0300

    Добавил вывод значений с приборов

---
+++ Assets/DebugInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugInfo : MonoBehaviour
+{
+    public Text Spring;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GlobalVariables.c != 1)
+        {
+            int l = GlobalVariables.c-1;
+            Spring.text="Выбран амортизатор № " + l.ToString();
+        } 
+        else
+        {
+            Spring.text="Амортизатор не установлен";
+        }
+    }
+}
