edc2fd5dd0db66b6fea1c4f1ae3a2eccb5431ccb
antonov-andrey144 <antonov-andrey144@yandex.ru>
2021-03-03 00:33:11 +0300

    Ползунок стал плавным, изменена логика скриптов по его перемещению

---
+++ Assets/Polzunok.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Polzunok : MonoBehaviour
+{
+
+    public Vector3 target;
+    public float smoothTime = 0.7F;
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        target.Set(transform.position.x, transform.position.y, transform.position.z);
+    }
+
+    void Update()
+    {
+        //Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
+
+        transform.position = Vector3.SmoothDamp(transform.position, target, ref velocity, smoothTime);
+    }
+
+    public void MoveTo(float position)
+    {
+        target.Set(transform.position.x, transform.position.y, position);
+    }
+
+    
+}
--- Assets/PolzunokClick.cs
+++ Assets/PolzunokClick.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Click450 : MonoBehaviour
+public class PolzunokClick : MonoBehaviour
 {
     GameObject go;
+
+    [SerializeField] int index;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,16 +15,12 @@ public class Click450 : MonoBehaviour
         Debug.Log(go.name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnMouseDown()
     {
         //go.transform.Translate(0, 0, transform.position.z);
-        go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, transform.position.z);
-        GlobalVariables.b = 1;
+        //go.transform.position = new Vector3(go.transform.position.x, go.transform.position.y, transform.position.z);
+        GlobalVariables.b = index;
+
+        ((Polzunok)go.GetComponent<Polzunok>()).MoveTo(transform.position.z);
     }
 }
