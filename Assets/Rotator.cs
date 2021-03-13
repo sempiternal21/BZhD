3827ff66a7a113ea3e1fcb29822a59b4395d27b5
sempiternal21 <danil.antonov.96@list.ru>
2021-03-14 02:02:55 +0300

    мелкие фиксы

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -1,23 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Rotator : MonoBehaviour
 {
     public static int pos = 1;
+    public float mouseSensitivity = 3.0f;
     float x = 0;
     float y = 0;
+    float rotationX = 0F;
+	float rotationY = 0F;
+    public float minimumX = -360F;
+	public float maximumX = 360F;
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+    Quaternion originalRotation;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F) angle += 360F;
+		if (angle > 360F) angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+
     // Update is called once per frame
+    public float speed = 5f;
+    public float speedRotateX = 2f;
+    public float speedRotateY = 2f;
+    public Transform Target;
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
 
+            }  
+    
+    
     }
+
 
     void OnMouseDown()
     {
